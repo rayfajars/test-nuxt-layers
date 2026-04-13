552f50c091ad3aa702d67511f1dd636946f414ff
Ray Fajar Salinggih <58242304+rayfajars@users.noreply.github.com>
2026-04-13 16:08:45 +0700

    chore: setup project nuxt layers for npm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  $meta: {
+    name: "test-nuxt-layers",
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
